dd9b9c8cc10183862e97175177e8e3ba02b36fcf
hyojeong0509 <41557022+hyojeong0509@users.noreply.github.com>
2026-02-14 22:42:11 +0900

    Feat: CSVLocalize 다중언어 시스템 추가

---
+++ Assets/Scripts/UI/LocalizationTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Localization.Settings;
+using TMPro;
+
+public class LocalizationTest : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown dropdown;
+
+    private void Start()
+    {
+        dropdown.onValueChanged.AddListener(UpdateLocale);
+    }
+
+    public void UpdateLocale(int index)
+    {
+        LocalizationSettings.SelectedLocale =
+            LocalizationSettings.AvailableLocales.Locales[index];
+
+        Debug.Log("언어 변경: " +
+            LocalizationSettings.SelectedLocale.name);
+    }
+}
